9712ee445bc17a4d48e05ba161eda4ce5a8d5a30
GuoXi <1206189299@qq.com>
2019-01-03 18:34:09 +0800

    ....

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import FastClick from 'fastclick'
+
+
+if ('addEventListener' in document) {
+    document.addEventListener('DOMContentLoaded', function(){
+        FastClick.attach(document.body)
+    },false)
+}
+
+const render = Component => {
+    ReactDOM.render(
+        <Provider store={store}>
+            <AppContainer>
+                <Component />
+            </AppContainer>
+        </Provider>,
+        document.getElementById('root')
+    )
+};
+
+render(Route);
+
+if (module.hot) {
+    module.hot.accept('./router/', () => {
+        render(Route);
+    })
+}
+
+registerServiceWorker();
